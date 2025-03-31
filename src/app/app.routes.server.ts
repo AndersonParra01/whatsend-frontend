67e9cf4703f5eb1bde8df85c6e39a514c06f78b1
Anderson <ajr.parra@yavirac.edu.ec>
2025-03-30 21:54:38 -0500

    add configuration routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -10,6 +10,22 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server
   },
   {
+    path: 'home',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'branches',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'dashboard',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'messages',
+    renderMode: RenderMode.Prerender
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
